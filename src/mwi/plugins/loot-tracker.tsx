d9b2404245e3615e753d20d3ab9c5d433cdd0a59
Xia <83057695+wang-x-xia@users.noreply.github.com>
2025-06-15 17:45:18 +0800

    Make Loot Tracker show hourly data or all data. (#19)

--- src/mwi/plugins/loot-tracker.tsx
+++ src/mwi/plugins/loot-tracker.tsx
@@ -6,6 +6,7 @@ import {getActionInputs, getActionName} from "../engine/action";
 import {resolveItemHrid} from "../engine/hrid";
 import {getLootLog} from "../engine/loot";
 import {LifecycleEvent, registerLifecycle} from "../lifecycle";
+import {saveSettings, useSettings} from "../settings";
 import {AddView} from "../view";
 
 export function lootTrackerPlugin() {
@@ -19,47 +20,53 @@ export function lootTrackerPlugin() {
 }
 
 function ShowLootTracker() {
-    const lootLogs = getLootLog().reverse();
-    return <table>
-        <thead>
-        <tr>
-            <th>Summary</th>
-            <th>Inputs</th>
-            <th>Spending</th>
-            <th>Spending/h</th>
-            <th>Drops</th>
-            <th>Income</th>
-            <th>Income/h</th>
-        </tr>
-        </thead>
-        <tbody>
-        {lootLogs.map((log) => <ShowLootLog key={log.startTime} log={log}/>)}
-        </tbody>
-    </table>;
+    const mode = useSettings<"all" | "hour">("loot-tracker.mode", "hour");
+
+    const lootLogs = [...getLootLog()].reverse();
+    return <>
+        <div>
+            Current: {mode === "hour" ? "Hourly Data" : "Total"}
+            <button onClick={() => saveSettings("loot-tracker.mode", "hour")}>Hourly</button>
+            <button onClick={() => saveSettings("loot-tracker.mode", "all")}>Total</button>
+        </div>
+        <table>
+            <thead>
+            <tr>
+                <th>Summary</th>
+                <th>Inputs</th>
+                <th>Spending</th>
+                <th>Drops</th>
+                <th>Income</th>
+            </tr>
+            </thead>
+            <tbody>
+            {lootLogs.map((log) => <ShowLootLog key={log.startTime} log={log} mode={mode}/>)}
+            </tbody>
+        </table>
+    </>
 }
 
-function ShowLootLog({log}: { log: LootLog }) {
+function ShowLootLog({log, mode}: { log: LootLog, mode: "all" | "hour" }) {
     const duration = (Date.parse(log.endTime) - Date.parse(log.startTime)) / 1000 / 60 / 60;
+    const factor = mode === "hour" ? duration : 1;
     const {total: income, items: drops} = prepareSellItems(Object.entries(log.drops)
-        .map(([hridHash, count]) => ({...resolveItemHrid(hridHash), count})))
+        .map(([hridHash, count]) => ({...resolveItemHrid(hridHash), count: count / factor})))
 
     const {total: spending, items: inputs} = prepareBuyItems(
         getActionInputs(log.actionHrid, log.primaryItemHash, log.secondaryItemHash)
-            .map((item) => ({...item, count: item.count * log.actionCount,})));
+            .map((item) => ({...item, count: item.count * log.actionCount / factor,})));
 
     const date = new Date(Date.parse(log.startTime));
 
     return <tr>
         <td>
             <div><b>{getActionName(log.actionHrid)}</b></div>
-            <div>{date.getFullYear()}-{date.getMonth() + 1}-{date.getDay()} {date.getHours()}:{date.getMinutes()}</div>
+            <div>{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()} {date.getHours()}:{date.getMinutes()}</div>
             <div><ShowNumber value={duration}/>h</div>
         </td>
         <td><ItemTable items={inputs}/></td>
         <td><ShowNumber value={spending}/></td>
-        <td><ShowNumber value={spending / duration}/></td>
         <td><ItemTable items={drops}/></td>
         <td><ShowNumber value={income}/></td>
-        <td><ShowNumber value={income / duration}/></td>
     </tr>
 }
