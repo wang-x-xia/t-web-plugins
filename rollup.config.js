170005f4904bed4ea44a05f9a95f9e427c285bcc
Wang Xia <wang_x_xia@163.com>
2025-06-29 19:05:08 +0800

    Use esm to reduce the size

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,27 @@ import typescript from "@rollup/plugin-typescript";
 import fs from 'fs/promises';
 import postcss from 'rollup-plugin-postcss'
 
+function esmGlobals(id) {
+    if (process.env.NODE_ENV === "production") {
+        // jsdelivr is faster
+        return `await import('https://cdn.jsdelivr.net/npm/${id}/+esm')`
+    } else {
+        return `await import('https://esm.sh/${id}?dev')`
+    }
+}
+
 export default {
     input: "src/mwi/main.ts",
     output: {
         format: "iife",
         file: "dist/mwi.js",
         banner: async (_) => await fs.readFile("src/mwi/main.ts.banner", {encoding: 'utf-8'}),
+        globals: {
+            "react": esmGlobals("react@19.1.0"),
+            "react-dom/client": esmGlobals("react-dom@19.1.0/client"),
+        },
     },
+    external: ["react", "react-dom/client"],
     plugins: [
         // Added for React
         commonjs(),
