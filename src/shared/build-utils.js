56d599e032698d8367c31c93755cde8a276c9afa
Xia <83057695+wang-x-xia@users.noreply.github.com>
2025-07-30 22:28:34 +0800

    Add deps for CDN module to unify the dependencies. (#50)

--- src/shared/build-utils.js
+++ src/shared/build-utils.js
@@ -17,21 +17,49 @@ function resolvePkgAndChild(id) {
 
 /**
  * Get the CDN url for a package
- * @param id
+ * @param id {string}
  * @return {string}
  */
 export function resolveCdnUrl(id) {
     const [pkg, child] = resolvePkgAndChild(id);
-    const pkgJson = readFileSync(path.resolve("node_modules", pkg, "package.json"), "utf8");
-    const {version} = JSON.parse(pkgJson);
-    let pkgUrl = `https://esm.sh/${pkg}@${version}`
+    const dependencies = {}
+    const selfVersion = collectVersion(pkg, dependencies)
+    let pkgUrl = `https://esm.sh/${pkg}@${selfVersion}`
     if (child) {
         pkgUrl += `/${child}`
     }
+    const searchParams = []
     if (!isProduction) {
-        pkgUrl += "?dev"
+        // Add dev flag
+        searchParams.push("dev")
+    }
+    if (Object.entries(dependencies).length > 0) {
+        // Add dependency versions
+        const deps = Object.entries(dependencies).map(([subPkg, version]) => `${subPkg}@${version}`).join(",")
+        searchParams.push(`deps=${deps}`)
+    }
+    if (searchParams) {
+        pkgUrl += "?" + searchParams.join("&")
     }
     return pkgUrl
+}
+
+/**
+ * Collect the package version information
+ *
+ * @param pkg {string} is the pkg
+ * @param versions {Record<string, string>} is the map of dependency version
+ * @return {string} pkg version
+ */
+function collectVersion(pkg, versions) {
+    const pkgJson = readFileSync(path.resolve("node_modules", pkg, "package.json"), "utf8");
+    const {version, dependencies = {}, peerDependencies = {}} = JSON.parse(pkgJson);
+    Object.keys({...dependencies, ...peerDependencies}).forEach(subPkg => {
+        if (versions[subPkg] === undefined) {
+            versions[subPkg] = collectVersion(subPkg, versions)
+        }
+    })
+    return version
 }
 
 /**
